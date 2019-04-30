c7ca78bce91ab62708ea07cbf576cc093093de3f
dingdekai <dekai.ding@shen021.com>
2019-04-30 14:42:19 +0800

    [Add] the logci about part of socket. next will add the send and receive parts.

--- src/network/SocketEngine.cpp
+++ src/network/SocketEngine.cpp
@@ -19,9 +19,73 @@ void SocketEngine::send(Binary* buffer)
 
 bool SocketEngine::connect(std::string ip, int port)
 {
-	_address.setIP(ip);
-	_address.setPort(port);
-	_socket.setIPAddress(_address);
+	if (_status == SocketStatus::SocketDisconnected)
+	{
+		_address.setIP(ip);
+		_address.setPort(port);
+		_socket.setIPAddress(_address);
+		if (_socket.connect())
+		{
+			registerDisposeSocketIO();
+			_status = SocketStatus::SocketConnecting;
+			return true;
+		}
+		else
+		{
+			_socket.close();
+			_status = SocketStatus::SocketDisconnected;
+		}
+	}
 	return true;
 }
 
+void SocketEngine::disconnect()
+{
+	unregisterDisposeSocketIO();
+	_socket.close();
+	_status = SocketStatus::SocketDisconnected;
+	onDisconnect();
+}
+
+void SocketEngine::disposeSocketIO(float dt)
+{
+	if (_status == SocketStatus::SocketConnecting)
+	{
+
+	}
+	else if (_status == SocketStatus::SocketConnected)
+	{
+
+	}
+	else
+	{
+
+	}
+}
+
+void SocketEngine::disposeRead()
+{
+
+}
+
+void SocketEngine::disposeWrite()
+{
+
+}
+
+void SocketEngine::registerDisposeSocketIO()
+{
+	if (_isSchedule)
+		return;
+	Director::getInstance()->getScheduler()->schedule(schedule_selector(disposeSocketIO), this, 0.0f, false);
+	_isSchedule = true;
+}
+
+void SocketEngine::unregisterDisposeSocketIO()
+{
+	if (_isSchedule)
+	{
+		Director::getInstance()->getScheduler()->unschedule(schedule_selector(disposeSocketIO), this);
+		_isSchedule = false;
+	}
+}
--- src/network/SocketEngine.h
+++ src/network/SocketEngine.h
@@ -15,14 +15,28 @@ public:
 	virtual ~SocketEngine();
 
 	virtual void onReceiveMessage(Binary& buffer) = 0;
+	virtual void onConnect() = 0;
+	virtual void onTimeout() = 0;
+	virtual void onDisconnect() = 0;
 	
 public:
 	void send(Binary* buffer);
 	bool connect(std::string ip, int port);
+	void disconnect();
+
+	void disposeSocketIO(float dt);
+
+	void disposeRead();
+	void disposeWrite();
+
+	void registerDisposeSocketIO();
+	void unregisterDisposeSocketIO();
 
 private:
 	NetAddress _address;
 	Socket _socket;
+	SocketStatus _status = SocketStatus::SocketDisconnected;
+	bool _isSchedule;
 };
 
 NS_CC_END
